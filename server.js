e08392e7819b3c78b737611461c1bebd7fa0b27b
Bruno Guazina <bruno@guazina.com>
2014-10-07 23:31:58 -0400

    ajustes na rota e formatações iniciais

--- server.js
+++ server.js
@@ -2,10 +2,19 @@ var http = require('http');
 var fs = require('fs');
 
 var request = function(req, res){
-	fs.readFile('index.html', function(erro, data){
-		res.writeHead(200);
-		res.end(data);
-	});
+	console.log(req.url);
+
+	if (req.url == '/')
+		fs.readFile('index.html', function(erro, data){
+			res.writeHead(200);
+			res.end(data);
+		});
+
+	else
+		fs.readFile(req.url.replace('/', ''), function(erro, data){
+			res.writeHead(200);
+			res.end(data);
+		});
 }
 
 var server = http.createServer(request);
